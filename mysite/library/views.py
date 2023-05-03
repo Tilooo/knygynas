8d31e8530b53105c53ba77de240d173cb47e1a69
Tilooo <moonaviad@gmail.com>
2023-05-03 15:18:11 +0300

    6: +Idetas autoriu puslapiavimas

--- mysite/library/views.py
+++ mysite/library/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Book, BookInstance, Author
 from django.views import generic
+from django.core.paginator import Paginator
 
 
 # Create your views here.
@@ -21,7 +22,10 @@ def index(request):
 
 
 def authors(request):
-    authors = Author.objects.all()
+    paginator = Paginator(Author.objects.all(), 4)
+    page_number = request.GET.get('page')
+    paged_authors = paginator.get_page(page_number)
+    authors = paged_authors
     context = {
         'authors': authors,
     }
