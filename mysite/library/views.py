b189c9c54828e616588a1b8b2c45fb64779afaf5
Tilooo <moonaviad@gmail.com>
2023-05-05 14:22:55 +0300

    7: +Idetas apsilankymu skaitiklis.

--- mysite/library/views.py
+++ mysite/library/views.py
@@ -12,12 +12,14 @@ def index(request):
     num_instances = BookInstance.objects.all().count()
     num_authors = Author.objects.count()
     num_instances_available = BookInstance.objects.filter(status__exact='g').count()
-
+    num_visits = request.session.get('nu_visits', 1)
+    request.session['num_visits'] = num_visits +1
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
         'num_authors': num_authors,
         'num_instances_available': num_instances_available,
+        'num_visits': num_visits,
     }
     return render(request, 'index.html', context=context)
 
