08b7b7fb0ba33e19c05962989e012e8290743b10
Tilooo <moonaviad@gmail.com>
2023-05-09 13:02:04 +0300

    8: +Pridetas HTML aprasymas prie autoriu.

--- mysite/library/models.py
+++ mysite/library/models.py
@@ -2,6 +2,7 @@ from django.db import models
 import uuid
 from django.contrib.auth.models import User
 from datetime import date
+from tinymce.models import HTMLField
 
 # Create your models here.
 class Genre (models.Model):
@@ -19,7 +20,7 @@ class Genre (models.Model):
 class Author(models.Model):
      first_name = models.CharField(verbose_name='Vardas', max_length=50)
      last_name = models.CharField(verbose_name='Pavarde', max_length=50)
-     description = models.TextField(verbose_name='Aprasymas', max_length=2888, default='')
+     description = HTMLField(verbose_name='Aprasymas', max_length=2888, default='')
 
      def display_books(self):
          return ', '.join(book.title for book in self.books.all())
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -26,5 +26,6 @@ urlpatterns = [
     path('library/', include('library.urls')),
     path('', RedirectView.as_view(url='library/', permanent=True)),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('tinymce/', include('tinymce.urls')),
 ] + (static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) +
     static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT))
