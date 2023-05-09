7d61c76a79f4d4dca71a051f305a6710f4883af9
Tilooo <moonaviad@gmail.com>
2023-05-09 16:45:58 +0300

    9: +Ideta vartotoju registracija.

--- mysite/library/urls.py
+++ mysite/library/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path('books/', views.BookListView.as_view(), name='books'),
     path('books/<int:pk>/', views.BookDetailView.as_view(), name='book'),
     path('search/', views.search, name='search'),
-    path('my_books/', views.MyBookInstanceListView.as_view(), name='my_books')
+    path('my_books/', views.MyBookInstanceListView.as_view(), name='my_books'),
+    path('register/', views.register, name='register'),
 ]
